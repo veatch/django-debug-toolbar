b16bca9b59f215fac4bc75cbaafdfa001146495b
David Cramer <dcramer@david-cramers-macbook.local>
2008-09-09 00:12:56 -0500

    added session variables to http globals

--- debug_toolbar/panels/http_vars.py
+++ debug_toolbar/panels/http_vars.py
@@ -9,7 +9,7 @@ class HttpVarsDebugPanel(DebugPanel):
     # List of headers we want to display
 
     def title(self):
-        return 'HTTP POST/GET'
+        return 'HTTP Globals'
 
     def url(self):
         return ''
@@ -17,6 +17,7 @@ class HttpVarsDebugPanel(DebugPanel):
     def content(self):
         context = {
             'get': [(k, self.request.GET.getlist(k)) for k in self.request.GET.iterkeys()],
-            'post': [(k, self.request.POST.getlist(k)) for k in self.request.POST.iterkeys()]
+            'post': [(k, self.request.POST.getlist(k)) for k in self.request.POST.iterkeys()],
+            'session': [(k, self.request.session.get(k)) for k in self.request.session.iterkeys()],
         }
         return render_to_string('debug_toolbar/panels/http_vars.html', context)
