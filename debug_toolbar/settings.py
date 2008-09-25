1ffffa9932fb30cd6f33fb25c9b13d9bb69a2a7a
David Cramer <dcramer@david-cramers-macbook.local>
2008-09-24 21:09:55 -0500

    disabled profiler as a default (some distros are missing profile modules)

--- debug_toolbar/settings.py
+++ debug_toolbar/settings.py
@@ -2,7 +2,7 @@ from django.conf import settings
 
 DEBUG_TOOLBAR_PANELS = getattr(settings, 'DEBUG_TOOLBAR_PANELS', (
     'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.profiler.ProfilerDebugPanel',
+    # 'debug_toolbar.panels.profiler.ProfilerDebugPanel',
     'debug_toolbar.panels.cache.CacheDebugPanel',
     #'debug_toolbar.panels.templates.DjangoTemplatesDebugPanel',
     'debug_toolbar.panels.headers.HeaderDebugPanel',
