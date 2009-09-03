8700eadfca8383e32c0f7f8b101b8c065ffb7278
David Cramer <dcramer@david-cramers-macbook.local>
2009-09-03 15:41:53 -0500

    Added setuptools script

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-debug-toolbar',
+    version='0.1',
+    url='http://github.com/dcramer/django-debug-toolbar/',
+    description = 'A debug toolbar overlay for Django.',
+    # author='David Cramer',
+    # author_email='dcramer@gmail.com',
+    install_requires=['django'],
+    packages=find_packages(),
+    include_package_data=True,
+)
